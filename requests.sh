bd8892f97f8a205bffd49039d1c6292e172ca25c
Karen Haining <khaining@cs.washington.edu>
2024-04-09 23:36:14 -0700

    use astrometry.net api

---
+++ requests.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+APIKEY=$1
+
+python3 client.py --apikey $APIKEY
+
+for f in ./raw_data/*
+do 
+    filename=$(basename -- "$f")
+    filename="${filename%.*}"
+    
+    resultsdir=./processed_data/$filename
+    mkdir -p ./processed_data/$filename
+
+    echo "Processing $f file..."
+    python3 client.py --apikey $APIKEY                      \
+                      --upload $f                           \
+                      --newfits $resultsdir/newfits.fits    \
+                      --corr    $resultsdir/corr.fits       \
+                      --private
+
+done
