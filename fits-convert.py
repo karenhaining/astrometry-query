5141f50e5315e5556f15cc1a6195b10bae71f5ea
Karen Haining <khaining@cs.washington.edu>
2024-04-10 11:47:00 -0700

    finishing fits trials

--- fits-convert.py
+++ fits-convert.py
@@ -12,12 +12,37 @@ from astropy.utils.data import get_pkg_data_filename
 fits_image_filename = get_pkg_data_filename('corr.fits')
 table_data = fits.getdata(fits_image_filename,ext=1)
 # print(table_data)
+u = Table.read('corr.fits')
+# print(u.info)
 
-# how to get 
+
+# how to get image data
 fits_image_filename2 = get_pkg_data_filename('new-image.fits')
 image_data = fits.getdata(fits_image_filename2,ext=0)
-print(image_data)
-print(image_data.shape)
+# print(image_data[0])
+# print(image_data.shape)
+# print(fits_image_filename2.info())
+print(fits.info(fits_image_filename2))
+# only the
+
+
+v = Table.read('rdls.fits')
+print(v.info)
+"""
+name  dtype  unit
+---- ------- ----
+  RA float64  deg
+ DEC float64  deg
+ """
+print(v)
+
+fits_image_filename2 = get_pkg_data_filename('wcs.fits')
+# image_data = fits.getdata(fits_image_filename2,ext=0)
+# print(image_data[0])
+# print(image_data.shape)
+# print(fits_image_filename2.info())
+print(fits.info(fits_image_filename2))
+
 
 # fits.info(fits_image_filename)
 
--- pics.py
+++ pics.py
@@ -1,26 +1,70 @@
 import os
 import subprocess
+import json
+import numpy as np
 import profile
 
 from astropy.io import fits
 from astropy.utils.data import get_pkg_data_filename
 
+
+# corr file headers:
+# field_x, field_y, field_ra, field_dec, index_x, index_y, index_ra, index_dec, 
+# index_id, field_id, match_weight, flux, background
+CORR_X_IDX = 0
+CORR_Y_IDX = 1
+CORR_ID_IDX = 8
+
 # send pics & obtain results from astrometry
 # subprocess.run(["./requests.sh", profile.API_KEY])
 
-# process results
-results_dir = './processed_data'
+# process results from astrometry
+processed_dir = './processed_data'
+results = {}
 
-subfolders = [ f.path for f in os.scandir(results_dir) if f.is_dir() ]
-for dir in os.scandir(results_dir):
+subfolders = [ f.path for f in os.scandir(processed_dir) if f.is_dir() ]
+for dir in os.scandir(processed_dir):
+    print(dir.name + " ===================")
+    file_res = {}
 
-    corr_filename = get_pkg_data_filename(results_dir + '/' + dir.name + '/corr.fits')
-    newfits_filename = get_pkg_data_filename(results_dir + '/' + dir.name + '/newfits.fits')
+    path = processed_dir + '/' + dir.name
 
+    # for coordinates + identities
+    corr_filename = get_pkg_data_filename(path + '/corr.fits')
     corr_data = fits.getdata(corr_filename,ext=1)
-    newfits_data = fits.getdata(newfits_filename,ext=0)
+    
+    # for actual pixel values
+    newfits_filename = get_pkg_data_filename(path + '/newfits.fits')
+    newfits_data = np.array(fits.getdata(newfits_filename,ext=0))
+
+    # if its rgb, convert to grayscale
+    if newfits_data.shape[0] == 3:
+        newfits_data = (newfits_data[0] + newfits_data[1] + newfits_data[2]) / 3
+    
+    centroids = []
+    stars = []
+    for star in corr_data:
+        x = star[CORR_X_IDX]
+        y = star[CORR_Y_IDX]
+        id = star[CORR_ID_IDX]
+
+        print(id)
+
+        centroids.append([x, y])
+
+
+    file_res['centroids'] = centroids
 
     
-
     print(corr_data.shape)
     print(newfits_data.shape)
+
+    print(corr_data[0])
+
+    results[dir.name] = file_res
+
+# write to json file
+json_results = json.dumps(results, indent=4)
+
+with open("results.json", 'w') as outfile:
+    outfile.write(json_results)
